7065ed9e343d8eb512514c985e41a9c2c7ba8229
Yar Storm <48162040+BenderRodriguez-Bending@users.noreply.github.com>
2022-03-23 22:51:54 +0300

    Update SettingsController.php

--- SettingsController.php
+++ SettingsController.php
@@ -5,10 +5,6 @@ namespace App\Http\Controllers\CONSOLIDATION;
 interface SettingsController
 {
     public const PRODUCTS = [
-        "AgrNoMite" => "Защита от клеща",
-    ];
-
-    public const URL_SLUG_NAME = [
-        "AgrNoMite" => "no_mite",
+        
     ];
 }
